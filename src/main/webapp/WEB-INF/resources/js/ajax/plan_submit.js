495f841d592f49e30bb9dca9e8d9caa8652f47b7
Troevil <304445056@qq.com>
2015-03-15 20:13:37 +0800

    add double click for select book

--- src/main/webapp/WEB-INF/resources/js/ajax/plan_submit.js
+++ src/main/webapp/WEB-INF/resources/js/ajax/plan_submit.js
@@ -63,6 +63,7 @@ $(function() {
 					function() {
 						$
 								.TeachDialog({
+									modalID : "SelectBooksModal",
 									title : 'Select Books From Table',
 									content : '<div class="searchClass row"><div class="col-xs-12"><label>Code</label><input class="SearchForm form-control" type="text" id="SearchCode" /> <label>Name</label><input class="SearchForm form-control" type="text" id="SearchName" /> <label>SN</label><input class="SearchForm form-control" type="text" id="SearchSN" /> <label>Type</label><select class="SearchForm form-control" id="SearchType"><option value="-1">All Type</option></select></div><div class="col-xs-12"><label>Press</label><input class="SearchForm form-control" type="text" id="SearchPress" /> <label>Author</label><input class="SearchForm form-control" type="text" id="SearchAuthor" /> <label>Supplier</label><select class="SearchForm form-control" id="SearchSupplier"><option value="-1">All Supplier</option></select> <label>Price</label><input class="SearchForm form-control" type="text" id="SearchPrice" /></div><div class="col-xs-12"><label>Discount</label><input class="SearchForm form-control" type="text" id="SearchDiscount" /> <label>Date</label><input class="SearchForm form-control" type="text" id="SearchDate" ReadOnly /><button id="Search" class="btn btn-primary btn-xs">Search</button></div></div><table id="datatable_bookinfo" style="margin-top:10px;"></table>',
 									largeSize : true,
@@ -183,6 +184,11 @@ $(function() {
 											onBeforeLoad : function(param) {
 												param = getSearchParams(param);
 											},
+											onDblClickRow : function(rowIndex, rowData) {
+												$('#BookId').empty();
+												$('#BookId').append('<option value="' + rowData.intbookid + '">' + rowData.strbookname + '</option>')
+												$('#SelectBooksModal').modal('hide');
+											}
 										});
 										$('#Search').click(function() {
 											$mydategrid.datagrid('reload');
